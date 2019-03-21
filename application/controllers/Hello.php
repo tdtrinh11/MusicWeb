65e0f51b6902780286edc35349b874376000f037
doandaitien <20166827@student.hust.edu.vn>
2019-03-21 08:02:26 +0700

    Them file Hello

---
+++ application/controllers/Hello.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class ControllerName extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		echo 'string';
+	}
+
+}
